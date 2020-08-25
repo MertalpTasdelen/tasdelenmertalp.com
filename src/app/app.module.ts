f485f8394f9558cdd1dfd42ffeb036887933b8e3
Mertalp Taşdelen <mertalptasdelen@Mertalps-Macbook-Pro.local>
2020-08-25 20:59:12 +0300

    Added new components for website UI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,16 +7,35 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NewsComponent } from './news/news.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { PodcastComponent } from './podcast/podcast.component';
+import { CrossfitComponent } from './crossfit/crossfit.component';
+
+const appRoutes: Routes = [
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'crossfit', component: CrossfitComponent },
+  { path: 'news'    , component: NewsComponent     },
+  { path: 'podcast' , component: PodcastComponent  },
+  { path: 'projects', component: ProjectsComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    NewsComponent,
+    ProjectsComponent,
+    ArticlesComponent,
+    PodcastComponent,
+    CrossfitComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/crossfit/crossfit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-crossfit',
+    templateUrl: './crossfit.component.html',
+    styleUrls: ['./crossfit.component.css']
+})
+export class CrossfitComponent implements OnInit{
+    constructor() {}
+
+    ngOnInit(): void {
+        
+    }
+}
