7c081bc14f282584537ebf5f82399f02fc6e249a
Mertalp Taşdelen <tasdelenmertalp@gmail.com>
2020-09-08 01:07:15 +0300

    Main page css and some podcast settings done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+ // Material Style Basic Audio Player Title and Audio URL
+  msbapTitle = 'Audio Title';
+  msbapAudioUrl = '/assets/episodes/TahtaMasaE5v0_1.mp3';   
+   
+  msbapDisplayTitle = false; 
+  msbapDisplayVolumeControls = true;  
+  // msbapDisablePositionSlider = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onEnded(eventData){
+    console.log("We can use that event to trigger to skip next episode!");
+  }
+  
 }
